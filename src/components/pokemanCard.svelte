0477eb96439cded5c882de81bea6fe3fe7df7eaa
failpark <daniel.matthes@noxa.de>
2021-08-23 22:25:18 +0200

    feat: remove fade & use avatar as favicon

--- src/components/pokemanCard.svelte
+++ src/components/pokemanCard.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-  import { fade } from 'svelte/transition';
   export let pokeman;
 </script>
 
 <a
   class="p-6 bg-gray-100 text-gray-800 text-center rounded-md shadow-sm hover:shadow-md flex flex-col items-center"
   href={`/pokemon/${pokeman.id}`}
-  transition:fade
 >
   <img class="h-30 w-30" src={pokeman.image} alt={pokeman.name} />
   <h2 class="uppercase text-xl">{pokeman.id}. {pokeman.name}</h2>
